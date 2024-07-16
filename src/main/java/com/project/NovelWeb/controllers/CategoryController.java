08d277d4bf1f173351846022c65384f1c67e2606
Tuan <tuan2807200x4@gmail.com>
2024-07-16 10:38:26 +0700

    update CategoryController

--- src/main/java/com/project/NovelWeb/controllers/CategoryController.java
+++ src/main/java/com/project/NovelWeb/controllers/CategoryController.java
@@ -1,30 +1,29 @@
 package com.project.NovelWeb.controllers;
 
 import com.project.NovelWeb.models.dto.requests.CategoryRequest;
+import com.project.NovelWeb.models.dto.responses.ResponseObject;
 import com.project.NovelWeb.models.entity.Novel.Category;
 import com.project.NovelWeb.models.dto.responses.CategoryResponse;
 import com.project.NovelWeb.services.impl.CategoryServiceImp;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("${api.prefix}/categories")
 public class CategoryController {
     private final CategoryServiceImp categoryServiceImp;
 
-    @PostMapping
+    @PostMapping("")
     public ResponseEntity<CategoryResponse> createCategory(
             @Valid @RequestBody CategoryRequest categoryRequest,
             BindingResult bindingResult) {
@@ -41,5 +40,51 @@ public class CategoryController {
         Category category = categoryServiceImp.createCategory(categoryRequest);
         categoryResponse.setCategory(category);
         return ResponseEntity.ok(categoryResponse);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<ResponseObject> getAllCategories(
+            @RequestParam("page")   int page,
+            @RequestParam("limit")   int limit) {
+        List<Category> categoryList = categoryServiceImp.getAllCategories();
+        return ResponseEntity.ok(ResponseObject.builder()
+                        .message("Get list of Categories successfully.")
+                        .data(categoryList)
+                        .status(HttpStatus.OK)
+                        .build());
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseObject> getCategoryById(
+            @PathVariable("id") Long id) {
+    Category category = categoryServiceImp.getCategoryById(id);
+    return ResponseEntity.ok(ResponseObject.builder()
+            .status(HttpStatus.OK)
+            .message("Get category information successfully.")
+            .data(category)
+            .build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ResponseObject> updateCategory(
+            @PathVariable("id") Long id,
+            @Valid @RequestBody CategoryRequest categoryRequest
+    ) {
+        categoryServiceImp.updateCategory(categoryRequest, id);
+        return ResponseEntity.ok(ResponseObject.builder()
+                        .message("UPDATE_CATEGORY_SUCCESSFULLY.")
+                        .data(categoryServiceImp.getCategoryById(id))
+                        .build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ResponseObject> deleteCategory(
+            @PathVariable("id") long id
+    ) throws ChangeSetPersister.NotFoundException {
+        categoryServiceImp.deleteCategory(id);
+        return ResponseEntity.ok(ResponseObject
+                        .builder()
+                        .status(HttpStatus.OK)
+                        .message("DELETE_CATEGORY_SUCCESSFULLY.")
+                        .build());
     }
 }
